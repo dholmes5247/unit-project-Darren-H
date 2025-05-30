37f35ea0c6435b681d6a256f8e3adc5a66133289
Darren-Holmes <dholmes5247@hotmail.com>
2025-05-30 16:32:17 -0500

    standard

--- unit-project-Darren-H/src/App.jsx
+++ unit-project-Darren-H/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
